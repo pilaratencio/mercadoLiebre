6cd852b16b70aadd5902643abbb04d6df6c7e37e
pilaratencio <pilaratencio@gmail.com>
2022-06-13 20:36:23 -0300

    mercadoLiebre1

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+app.set("puerto",process.env.PORT || 3001)
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+app.listen(app.get("puerto"), ()=>{
+    console.log('Servidor funcionando');});
